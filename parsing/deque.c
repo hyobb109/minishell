3b3239ac199215e35d16ec9d3ad00c26a94f7a7f
Hyobin Cho <hyobicho@c1r7s6.42seoul.kr>
2023-04-25 21:27:21 +0900

    env deque update

--- parsing/deque.c
+++ parsing/deque.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   deque.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: hyobicho <hyobicho@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 14:23:22 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/04/25 19:55:22 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/04/25 21:26:45 by hyobicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,29 @@
 // 	deque->cnt++;
 // }
 
+// void	append_front_env(t_edeque *deque, char *command)
+// {
+// 	t_env	*new_element;
+
+// 	new_element = (t_env *)malloc(sizeof(t_env));
+// 	if (!new_element)
+// 		return ;
+// 	init_element(new_element, command);
+// 	if (!deque->head)
+// 	{
+// 		deque->head = new_element;
+// 		deque->tail = new_element;
+// 	}
+// 	else
+// 	{
+// 		deque->head->prev = new_element;
+// 		new_element->next = deque->head;
+// 		deque->head = new_element;
+// 	}
+// 	deque->cnt++;
+// }
+
+
 void	append_back(t_deque *deque, t_token *token)
 {
 	if (!deque->tail)
@@ -46,6 +69,22 @@ void	append_back(t_deque *deque, t_token *token)
 		token->prev = deque->tail;
 		deque->tail->next = token;
 		deque->tail = token;
+	}
+	deque->cnt++;
+}
+
+void	append_back_env(t_edeque *deque, t_env *env)
+{
+	if (!deque->tail)
+	{
+		deque->head = env;
+		deque->tail = env;
+	}
+	else
+	{
+		env->prev = deque->tail;
+		deque->tail->next = env;
+		deque->tail = env;
 	}
 	deque->cnt++;
 }
@@ -72,9 +111,53 @@ t_token	*pop_front(t_deque *deque)
 	return (0);
 }
 
+t_env	*pop_front_env(t_edeque *deque)
+{
+	t_env	*tmp;
+
+	if (deque->cnt)
+	{
+		tmp = deque->head;
+		deque->head = tmp->next;
+		if (deque->head)
+			deque->head->prev = 0;
+		tmp->next = 0;
+		deque->cnt--;
+		if (deque->cnt == 0)
+		{
+			deque->head = 0;
+			deque->tail = 0;
+		}
+		return (tmp);
+	}
+	return (0);
+}
+
 t_token	*pop_back(t_deque *deque)
 {
 	t_token	*tmp;
+
+	if (deque->cnt)
+	{
+		tmp = deque->tail;
+		deque->tail = tmp->prev;
+		if (deque->tail)
+			deque->tail->next = 0;
+		tmp->prev = 0;
+		deque->cnt--;
+		if (deque->cnt == 0)
+		{
+			deque->head = 0;
+			deque->tail = 0;
+		}
+		return (tmp);
+	}
+	return (0);
+}
+
+t_env	*pop_back_env(t_edeque *deque)
+{
+	t_env	*tmp;
 
 	if (deque->cnt)
 	{
