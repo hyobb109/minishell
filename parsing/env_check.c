a62aefd5e774d5aea8dc141d4584bfaa2c9ef031
hyobin <hyobb109@gmail.com>
2023-04-29 02:57:10 +0900

    parameter updated && add comments

--- parsing/env_check.c
+++ parsing/env_check.c
@@ -12,9 +12,10 @@
 
 #include "../minishell.h"
 
-int	is_envkey(const char *s1, const char *s2, size_t *key_len)
+// strcmp 변형, 종료조건만 환경변수가 끝나는 조건으로 추가하고 str 인덱스를 증가시킴.
+int	is_envkey(const char *s1, const char *s2, int *key_len)
 {
-	size_t	i;
+	int		i;
 	char	*str;
 	char	*key;
 
@@ -22,9 +23,10 @@ int	is_envkey(const char *s1, const char *s2, size_t *key_len)
 	key = (char *)s2;
 	i = 0;
 
-	//환경변수 끝나는 조건 - 공백, <>, 작따,큰따
+	//환경변수 끝나는 조건 - 공백, <>, 작따,큰따, 널
 	while ((str[i] != '$' && !is_blank(str[i]) && str[i] != '\'' && str[i] != '\"' && str[i] != '<' && str[i] != '>' && str[i]) || key[i])
 	{
+		// 환경변수명과 키가 일치하지 않을 때, 그 잘못된 환경변수명만큼 길이만 늘려줌. ex) $USERa 면 5글자
 		if (str[i] != key[i])
 		{
 			while (str[i] != '$' && !is_blank(str[i]) && str[i] != '\'' && str[i] != '\"' && str[i] != '<' && str[i] != '>' && str[i])
@@ -41,17 +43,18 @@ int	is_envkey(const char *s1, const char *s2, size_t *key_len)
 	return (1);
 }
 // $USER hyobicho
-// 달러 뒤에 부터 보면서 정상적인 환경변수인지 찾고, 찾으면 memcpy로 붙여 넣어주고 str의 길이를 리턴.
-int	env_trans(char *str, int i, t_edeque *envp, char *buf)
+// 달러 뒷글자부터 보면서 정상적인 환경변수인지 찾고, 찾으면 memcpy로 붙여 넣어주고 str의 길이를 리턴.
+int	env_trans(char *str, t_edeque *envp, char *buf)
 {
 	t_env	*tmp;
-	size_t	key_len;
+	int		key_len;
 
 	key_len = 0;
+	// 환경변수 저장된 덱 돌면서 key 찾음.
 	tmp = envp->head;
 	while (tmp)
 	{
-		if (is_envkey(str + i, tmp->key, &key_len))
+		if (is_envkey(str, tmp->key, &key_len))
 		{
 			ft_memcpy(buf,tmp->val,ft_strlen(tmp->val));
 			return (key_len);
@@ -59,7 +62,7 @@ int	env_trans(char *str, int i, t_edeque *envp, char *buf)
 		//못찾으면 다음 환경변수 검사
 		tmp = tmp->next;
 	}
-	// printf("key not found.. key_len: %zu\n", key_len);
+	// printf("key not found.. key_len: %d\n", key_len);
 	return (key_len);
 }
 // 넘어올 때 큰따옴표를 만나거나, 따옴표 없는 환경변수 만나면 그 주소 넘어 옴.
@@ -72,29 +75,28 @@ int	search_env(char **str, char *buf, t_edeque *envp, int quote)
 
 	// 환경변수 끝나는 조건 - 공백, <>, 작따,큰따
 	// 따옴표 없을 때	
-		// str == $USER 
-		// buf == seulee2
-		//버퍼에 복사되고, str은 환경변수 끝에 포인터 위치.
-	// }
+	// str == $USER 
+	// buf == seulee2
+	//버퍼에 복사되고, str은 환경변수 끝에 포인터 위치.
 	if (!quote)
 	{
 		// 따옴표가 없는 경우는 $USER 면 U 의 주소부터 보내서 검사함.
-		*str += env_trans(*str, 1, envp, &buf[0]) + 1;
+		*str += env_trans(*str + 1, envp, &buf[0]) + 1;
 	}
 	else
 	{
 		i = 0;
-		printf("*str: %s, **str: %c\n", *str, **str);
+		// printf("*str: %s, **str: %c\n", *str, **str);
 		// "hello $USER bye.." 의 경우 => $만나면 그 다음 주소부터 env_trans 함수로 보내서 치환하기
 		while (**str != quote)
 		{
 			if (**str == '$')
 			{
 				// printf("*str: %s\n", *str);
-				key_len = env_trans(*str, 1, envp, &buf[i]);
+				key_len = env_trans(*str + 1, envp, &buf[i]);
 				// printf("ket_len: %d\n", key_len);
 				i = ft_strlen(buf);
-				*str += key_len + 1;
+				*str += key_len + 1; // $도 넘겨줘야하니까 key_len + 1 만큼 주소 넘겨줌
 				// printf("*******str: %s\n", *str);
 			}
 			else
