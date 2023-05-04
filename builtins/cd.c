6ff41b4406eeadc414a7902b320d64beda4215e6
Yunjoo Cho <yunjcho@c1r7s5.42seoul.kr>
2023-05-04 22:39:04 +0900

    Update cd()

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: yunjcho <yunjcho@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 16:28:59 by yunjcho           #+#    #+#             */
-/*   Updated: 2023/05/04 18:58:49 by yunjcho          ###   ########.fr       */
+/*   Updated: 2023/05/04 22:37:12 by yunjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 char	*init_destpath(t_token *token, char *cwd_name)
 {
 	size_t	target_idx;
-	char	*home_dir;
 	char	*result;
 	char	*result2;
 	char	**tmp;
@@ -23,45 +22,37 @@ char	*init_destpath(t_token *token, char *cwd_name)
 
 	(void) cwd_name;
 	target_idx = -1;
-	home_dir = ft_getenv(token->envp, "HOME");
 	result = ft_strdup("");
 	result2 = NULL;
 	tmp = NULL;
-	if (exist_args(token))
-	{
-		tmp = ft_split(token->command[1], '/');
-		idx = 0;
-		while (tmp[idx])
-		{ 
-			if (idx == 0)
-			{
-				if (!ft_strcmp(tmp[0], "~"))
-					result2 = ft_strjoin(result, home_dir);
-				else
-					result2 = ft_strjoin_three(result, "/", tmp[idx]);
-				free(result);
-				result = result2;
-				result2 = NULL;
-			}
+	tmp = ft_split(token->command[1], '/');
+	idx = 0;
+	while (tmp[idx])
+	{ 
+		if (idx == 0)
+		{
+			if (!ft_strcmp(tmp[0], "~"))
+				result2 = ft_strjoin(result, ft_getenv(token->envp, "HOME"));
 			else
-			{
 				result2 = ft_strjoin_three(result, "/", tmp[idx]);
-				free(result);
-				result = result2;
-				result2 = NULL;
-			}
-			idx++;
+			free(result);
+			result = result2;
+			result2 = NULL;
 		}
-		free_strs(tmp);
-		return (result);
+		else
+		{
+			result2 = ft_strjoin_three(result, "/", tmp[idx]);
+			free(result);
+			result = result2;
+			result2 = NULL;
+		}
+		idx++;
 	}
-	else
-	{
-		free_strs(tmp);
-		return (ft_strdup(home_dir));
-	}
+	free_strs(tmp);
+	return (result);
 }
 
+//TODO - 수정
 void	change_env(t_token *token, char *dest)
 {
 	char	*oldpwd;
@@ -95,19 +86,44 @@ int	exec_cd(t_token *token)
 {
 	char	*dest;
 	char	cwd_name[PATH_MAX];
+	char	*free_str;
+	t_env	*old_pwd;
+	t_env	*cur_pwd;
 
-	getcwd(cwd_name, sizeof(cwd_name));
-	dest = init_destpath(token, cwd_name);
+	old_pwd = NULL;
+	free_str = NULL;
+	if (exist_args(token))
+	{
+		if (ft_strchr_idx(token->command[1], '~') >= 0)
+			dest = init_destpath(token, cwd_name);
+		else
+		{
+			dest = ft_strdup(token->command[1]);
+			if (ft_strlen(dest) > 1 && dest[ft_strlen(dest) - 1] == '/')
+				dest[ft_strlen(dest) - 1] = '\0';
+		}
+	}
+	else
+		dest = ft_strdup(ft_getenv(token->envp, "HOME"));
+	printf("dest : %s\n", dest);
 	if (chdir(dest) == -1)
 	{
 		ft_dup2(STDERR_FILENO, STDOUT_FILENO);
 		printf("minishell: %s: %s: %s\n", token->command[0], \
-			token->command[1], strerror(errno));
+			dest, strerror(errno));
 		free(dest);
 		token->status = 1;
 		return (1);
 	}
-	change_env(token, dest);
+	old_pwd = find_value(token->envp, "OLDPWD");
+	free_str = old_pwd->val;
+	if (free_str)
+		free(free_str);
+	cur_pwd = find_value(token->envp, "PWD");
+	old_pwd->val = ft_strdup(cur_pwd->val);
+	free(cur_pwd->val);
+	getcwd(cwd_name, sizeof(cwd_name));
+	cur_pwd->val = ft_strdup(cwd_name);
 	free (dest);
 	return (1);
 }
